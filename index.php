129a9f05b9bbb76fa44d0b05dca5fc2bc4d78148
Edward <edwardnovak55@gmail.com>
2019-03-06 13:34:43 -0500

    Price Changed

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <h4 class="my-0 font-weight-normal">Free</h4>
       </div>
       <div class="card-body">
-        <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+        <h1 class="card-title pricing-card-title">$1 <small class="text-muted">/ mo</small></h1>
         <ul class="list-unstyled mt-3 mb-4">
           <li>10 users included</li>
           <li>2 GB of storage</li>
