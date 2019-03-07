3fa2d9115da76c5ef090fd40e0d51e58270c120b
Edward <edwardnovak55@gmail.com>
2019-03-07 14:01:44 -0500

    Added prepared statements into register

--- inc/register.php
+++ inc/register.php
@@ -1,20 +1,25 @@
 <?php
+//Adds the database connection
 include('db.php');
 
+//Gets results from form
 $fname = $_POST["fname"];
 $lname = $_POST["lname"];
 $username = $_POST["username"];
-$email = $_POST["email"];
+$username = $_POST["email"];
 $password = $_POST["password"];
+
+//This hashes the password so its not readable in the database.
+$hash = password_hash($password, PASSWORD_DEFAULT);
 
 try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO users (fname, lname, username, email, password)
-
-    VALUES ('$fname', '$lname', '$username', '$email', '$password')";
-    // use exec() because no results are returned
-    $conn->exec($sql);
+    //this prepares the sql statement to make sure no one can hack it.
+    $stmt = $conn->prepare("INSERT INTO users SET fname=?, lname=?, username=?, email=?, password=?");
+    //Adds the users data into the SQL and pushes to the database+
+    $stmt->execute([$fname, $lname, $username, $username , $hash ]);
+    
     echo "New record created successfully";
     }
 catch(PDOException $e)
